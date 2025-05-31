9cb41ee196c8cbe9b9a2726fd475de4ae54ec813
ctph <curwentph@gmail.com>
2025-05-31 12:00:57 -0500

    Update project

---
+++ 24-cp/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './layout/HomePage';
+import SimilarityPage from './layout/SimilarityPage';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/similarity/:pdbId" element={<SimilarityPage />} />
+      <Route path="*" element={<div>404 Not Found</div>} />
+    </Routes>
+  );
+};
+
+export default App;
